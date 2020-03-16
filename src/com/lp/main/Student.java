ffdbf035b34641ee2e71f2aeb54f90618bfaac2a
梁鹏 <liangp@gantsoftware.com>
2020-03-16 10:30:48 +0800

    i am student lp

--- src/com/lp/main/Student.java
+++ src/com/lp/main/Student.java
@@ -5,6 +5,7 @@ public class Student {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("i am student");
+      System.out.println("i am student lp ");
 	}
 
 }
