1e0a6b14da0ec080a1a7b08adb3b4be4e41d3319
梁鹏 <liangp@gantsoftware.com>
2020-03-19 11:06:21 +0800

    System.out.println(a==b);

--- src/com/lp/main/Student.java
+++ src/com/lp/main/Student.java
@@ -4,10 +4,11 @@ public class Student {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println("i am student");
-      System.out.println("i am student lp ");
-      System.out.println("i am student lp ");
-      System.out.println("i am student lp ");
+		
+		
+		int a =123456;
+		int b =123456;
+		System.out.println(a==b);
 	}
 
 }
