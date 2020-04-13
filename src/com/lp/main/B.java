86c9d2c1db45d54a12e9ea5f6e335fbe39bb156f
18040574336 <48577317+18040574336@users.noreply.github.com>
2020-04-13 22:15:03 +0800

    Update AB.java

--- src/com/lp/main/B.java
+++ src/com/lp/main/B.java
@@ -4,9 +4,10 @@ public class B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(11);
-System.out.println(22);
-System.out.println(33);
+		System.out.println(11);
+		System.out.println(22);
+		System.out.println(33);
+		System.out.println(55);
 	}
 
 }
