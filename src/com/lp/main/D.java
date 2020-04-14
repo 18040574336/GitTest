2b26329d38f0ac86f6cdd2e5481d471931665c96
liangpeng <liangp@gantsoftware.com>
2020-04-14 21:13:31 +0800

    更新d文件

--- src/com/lp/main/D.java
+++ src/com/lp/main/D.java
@@ -5,7 +5,7 @@ public class D {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println(5);
-       System.out.println(6);
+       System.out.println(62);
 	}
 
 }
