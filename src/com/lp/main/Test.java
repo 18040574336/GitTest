1492f76d8a9fbb9a389ef3be913779e7c0bc962e
username1 <username1@qq.com>
2020-06-15 12:20:37 +0800

    Revert "fix(零件目标价发布): 零件选择器过滤出类型是目标价的"
    This reverts commit 0890d3fd

--- src/com/lp/main/Test.java
+++ src/com/lp/main/Test.java
@@ -4,8 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Double a =1.2d;
-		Long b =1L;
+
+		System.out.println("init by lp1");
 		System.out.println("1 by lp1");
 		System.out.println("2 by l2p1");
 		System.out.println("3 by lp2");
